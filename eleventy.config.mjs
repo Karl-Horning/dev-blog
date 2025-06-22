998d1ce915fe0652286e58e591692703d4540369
Karl Horning <k.horning@me.com>
2025-06-22 21:58:45 +0100

    Combine `.eleventy.js` with `eleventy.config.mjs`

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,21 +1,43 @@
+import slugify from "slugify";
+
 export default function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src", {
-        filter: [
-            "404.html",
-            "**/*.css",
-            "!**/*tailwind-input.css",
-            "**/*.js",
-            "**/*.json",
-            "!**/*.11ty.js",
-            "!**/*.11tydata.js",
-        ],
+    // Filters
+    eleventyConfig.addFilter("slug", (str) =>
+        slugify(str, {
+            lower: true,
+            replacement: "-",
+            remove: /[*+~.()'"!:@]/g,
+        })
+    );
+
+    // Collections
+    eleventyConfig.addCollection("post", function (collectionApi) {
+        return collectionApi
+            .getFilteredByTag("post")
+            .sort((a, b) => b.date - a.date);
     });
 
-    eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addCollection("tagList", function (collectionApi) {
+        const tagSet = new Set();
+        collectionApi.getAll().forEach((item) => {
+            if ("tags" in item.data) {
+                let tags = Array.isArray(item.data.tags)
+                    ? item.data.tags
+                    : [item.data.tags];
+                tags.filter(
+                    (tag) => !["all", "nav", "post"].includes(tag)
+                ).forEach((tag) => tagSet.add(tag));
+            }
+        });
+        return [...tagSet].sort();
+    });
 
+    // Passthrough copy
+    eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.setServerPassthroughCopyBehavior("copy");
 
     return {
+        pathPrefix: "/dev-blog/",
         dir: {
             input: "src",
             output: "dist",
